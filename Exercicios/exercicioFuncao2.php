541f4bb95ceb7f39e8e4254639f92310488220c0
Milton-Pires <mpnn2207@gmail.com>
2025-09-02 21:51:11 -0300

    Subindo update clube

--- Exercicios/exercicioFuncao2.php
+++ Exercicios/exercicioFuncao2.php
@@ -5,30 +5,36 @@ INSS:14%
 IR - TABELA DE IR 2025 (27,5%)
 VALOR BRUTO: R$6300
 MOSTRAR NO FINAL:
-SALARIO FOLHA:
-DESCONTO INSS:
-DESCONTO IR:
-SALARIO LIQUIDO:
+SALARIO FOLHA;
+DESCONTO INSS;
+DESCONTO IR;
+DESCONTO CLUBE;
+SALARIO LIQUIDO;
 */
 
-$desc = 0; $valorBruto = 2500; $valorLiquido = 0; $porc = 0;
+$desc = 0; $valorBruto = 1500; $valorLiquido = 0; $porc = 0;
 
 //Descobrindo porcentagem de desconto
 function porcIR($valorBruto){
    if ($valorBruto < 2259.20){
-    return 0;
+        $porc = 0;
+        return $porc;
     }
     else if($valorBruto >= 2259.21 && $valorBruto <= 2826.65 ){
-    return 0.075;
+        $porc = 7.5/100;
+        return $porc;
     }
     else if($valorBruto >= 2826.66 && $valorBruto <= 3751.05){
-    return 0.15;
+        $porc = 15/100;
+        return $porc;
     }
     else if ($valorBruto >= 3751.06 && $valorBruto < 4664.68){
-    return 0.225;
+        $porc = 22.5/100;
+        return $porc;
     }
     else{
-    return 0.275;
+        $porc = 27.5/100;
+        return $porc;
     } 
 }
 
@@ -47,13 +53,37 @@ function descIR($valorBruto){
     return $desc;
 }
 $mostraIR = descIR($valorBruto);
+
+//Desconto clube:
+
+$esClube = "C"; $valorClube = 0;
+
+function desClube($esClube){
+    if($esClube == "A"){
+        $valorClube = 50;
+    }
+    else if ($esClube == "B"){
+        $valorClube = 30;
+    }
+    else if ($esClube == "C"){
+        $valorClube = 20;
+    }
+    else{
+        return $valorClube;
+    }
+    return $valorClube;
+
+}
+$mostraClube = desClube($esClube);
+
 //Valor liquido:
 
-$valorLiquido = $valorBruto - ($mostra + $mostraIR);
+$valorLiquido = $valorBruto - $valorClube - ($mostra + $mostraIR);
 
 //Mostrando o final:
 echo("O seu salario bruto é $valorBruto <br />");
 echo("O desconto do INSS é de $mostra <br />");
-echo ("O desconto do IR é de $mostraIR <br />");
+echo("O desconto do IR é de $mostraIR <br />");
+echo("O desconto do clube $esClube é $mostraClube <br />");
 echo("O valor liquido é $valorLiquido");
 ?>

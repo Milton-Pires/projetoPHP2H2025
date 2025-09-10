8ed89004268d31ebd27d89c87737d86db40cfe2f
Milton-Pires <mpnn2207@gmail.com>
2025-09-09 21:45:58 -0300

    Exercicio 4

--- Exercicios/exercicioRepeticao4.php
+++ Exercicios/exercicioRepeticao4.php
@@ -5,9 +5,51 @@ Criar um aplicativo que possibilite entrar com 3 números e possa imprimi-los em
 decrescente (insira números diferentes).
 
 */
+$n1 = 20; $n2 = 3; $n3 = 10;
 
-$n1 = 10; $n2 = 15; $n3 = 12;
+$maior = 0; $menor = 0; $meio = 0;
 
+$arrumado = FALSE;
 
+while($arrumado == FALSE){
+    if($n1>$n2 && $n1>$n3){
+        $maior = $n1;
+        if($n2>$n3){
+            $meio = $n2;
+            $menor = $n3;
+        }
+        else{
+            $meio = $n3;
+            $menor = $n2;
+        }
+        $arrumado = TRUE;
+    }
+    
+    else if($n2>$n1 && $n2>$n3){
+        $maior = $n2;
+        if($n1>$n3){
+            $meio = $n1;
+            $menor = $n3;
+        }
+        else{
+            $menor = $n1;
+            $meio = $n3;
+        }
+        $arrumado = TRUE;
+    }
+    else{
+        $maior = $n3;
+        if($n1>$n2){
+            $meio = $n1;
+            $menor = $n2;
+        }
+        else{
+            $menor = $n1;
+            $meio = $n2;
+        }
+        $arrumado = TRUE;
+    }
+}
+echo("Em ordem decrescente fica: $maior, $meio, $menor");
 
 ?>
--- Exercicios/exercicioRepeticao5.php
+++ Exercicios/exercicioRepeticao5.php
@@ -1,4 +1,12 @@
 <?php
+/*
+Faça um programa em PHP que leia a altura, a idade e o sexo de 50 pessoas e forneça
+as seguintes informações:
+A maior altura e a menor altura;
+A média de altura de mulheres;
+A idade do homem mais velho e da mulher mais nova.
+
+*/
 
 
 

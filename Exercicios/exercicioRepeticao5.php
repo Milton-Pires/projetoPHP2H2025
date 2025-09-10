d748325f8caba4f73e7eb28487f6d3bdab77e6fd
Milton-Pires <mpnn2207@gmail.com>
2025-09-09 22:17:33 -0300

    Inicio exercicio 5

--- Exercicios/exercicioRepeticao5.php
+++ Exercicios/exercicioRepeticao5.php
@@ -8,6 +8,27 @@ A idade do homem mais velho e da mulher mais nova.
 
 */
 
+//EXPLICANDO:
+/*Pega um contador e cada altura seria como um INPUT então dentro do while a gnt pede a altura e guarda ela numa variavel temporaria, e depois compara a variavel temporaria
+com a nova altura digitada sabendo qual a maior altura e depois compara com a maior altura anterior e vai substituindo, e por ultimo aumenta o contador para continuar o ciclo */
+$cont = 0;
+while($cont < 50){
+    $alt1 = 105;
+    $temp = $alt1;
+    $alt2 = 103;
+    if($temp >= $alt1){
+        $maioralt = $temp;
+        $alt1 = $menoralt;
+    };
+    if($maioralt <= $temp){
+        $maioralt = $temp;
+    }
+    
+    $cont++;
+}
+echo("A maior altura é de $maioralt centimetros");
+
+
 
 
 ?>

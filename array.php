0e54e9da6259ebb2fb1f197fb367fa3f769851ae
Milton-Pires <mpnn2207@gmail.com>
2025-09-23 20:20:51 -0300

    Subindo array

--- array.php
+++ array.php
@@ -1,5 +1,12 @@
 <?php
 //Percorrendo vetores
+$vetor = array("Maria", "João" , "Pedro" , "José", "Rafael", "Gabriel");
 
+for($i = 0; $i < 6 ; $i++){
+    $vetor[i] = 12;
+}
+for($i = 0; $i < 6 ; $i++){
+    echo("O valor do vetor: $vetor");
+}
 
 ?>

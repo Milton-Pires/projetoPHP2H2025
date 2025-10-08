16ddcd11b58147579c1b981ef4a9b67afdc40809
Milton=Pires <mpnn2207@gmail.com>
2025-10-07 21:18:16 -0300

    exercicio 5

--- Exercicios/Atividade 4 (Arrays)/ex05.php
+++ Exercicios/Atividade 4 (Arrays)/ex05.php
@@ -1,5 +1,5 @@
 <?php
-$cadastro = [
+$cadastros = [
     ["nome" => "Muriel", "cidade" => "São Paulo" , "idade" => 16 , "sexo" => "F"],
     ["nome" => "João", "cidade" => "São Paulo" , "idade" => 17 , "sexo" => "M"],
     ["nome" => "Thales", "cidade" => "Osasco" , "idade" => 19 , "sexo" => "M"],
@@ -9,23 +9,38 @@ $cadastro = [
     ["nome" => "Bruna", "cidade" => "Osasco" , "idade" => 16 , "sexo" => "F"],
     ["nome" => "Rodrigo", "cidade" => "São Paulo" , "idade" => 18 , "sexo" => "M"],
     ["nome" => "Helena", "cidade" => "Santos" , "idade" => 17 , "sexo" => "F"],
-    ["nome" => "Lyz", "cidade" => "Belo Horizonte" , "idade" => 17 , "sexo" => "F"],
+    ["nome" => "Lyz", "cidade" => "Santos" , "idade" => 17 , "sexo" => "F"],
 ];
 
 //TODOS OS NOMES E IDADES:
-echo("<pre>");
-var_dump($cadastro["nome"]);
-echo("</pre> <br />");
+foreach($cadastros as $cadastro){
+    echo ("Nome: " . $cadastro["nome"] . "- Idade: " . $cadastro["idade"] . " <br />");
+}
+echo("<br />");
 
 //Nomes de quem mora em Santos
-
-if($cadastro["cidade"] == "Santos"){
-    echo("<pre>");
-    var_dump($cadastro["nome"]);
-    echo("</pre> <br />");
+foreach($cadastros as $cadastro){
+    if($cadastro["cidade"] == "Santos"){
+        echo($cadastro["nome"]." Mora em Santos <br />");
+    }
+    
 }
+echo("<br />");
 
+//Quem tem mais de 18 anos
+foreach($cadastros as $cadastro){
+    if($cadastro["idade"] >= 18){
+        echo($cadastro["nome"] . " tem " . $cadastro["idade"] . "anos <br />");
+    }
+}
+echo("<br />");
+//Cadastro do sexo masculino
 
+foreach($cadastros as $cadastro){
+    if($cadastro["sexo"] == "M"){
+        echo($cadastro["nome"] . " é do sexo masculino <br />");
+    }
+}
 
 
 ?>

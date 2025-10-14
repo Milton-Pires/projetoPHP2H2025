805898a1ae5b4c7bdb016173ebb5b82715118eed
Milton-Pires <mpnn2207@gmail.com>
2025-10-14 19:20:30 -0300

    forms

---
+++ forms.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formularios</title>
+</head>
+<body>
+    <h1>Formularios</h1>
+    <form action="#" method="get">
+        <label for="nome">Nome:</label> 
+        <input type="text" name="nome" id="nome">
+        <br><br>    
+        <label for="email">Email:</label> 
+        <input type="email" name="email" id="email">
+        <br><br>
+        <input type="submit" name="enviar">
+        <button type="submit">Cadastrar</button>
+    </form>
+
+</body>
+</html>

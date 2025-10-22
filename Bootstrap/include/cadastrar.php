c48970d67e6ab70d5c77018e51d2a1b086101e55
Milton-Pires <mpnn2207@gmail.com>
2025-10-21 22:08:24 -0300

    Finalizando aula

--- Bootstrap/include/cadastrar.php
+++ Bootstrap/include/cadastrar.php
@@ -1,13 +1,21 @@
 <?php
 
-    $primeiroNome = $_POST['primeiroNome'];
-    $sobrenome = $_POST['Sobrenome'];
-    $escolhaPets = $_POST['escolhaPets'];
 
-    echo("$primeiroNome - $sobrenome - $escolhaPets <br>");
+    if($_SERVER['REQUEST_METHOD'] == "POST"){
+        $primeiroNome = $_POST['primeiroNome'];
+        $sobrenome = $_POST['Sobrenome'];
+        $escolhaPets = $_POST['escolhaPets'];
+
+        echo("$primeiroNome - $sobrenome - $escolhaPets <br>");
     
-    echo '<a href="../index.php">Voltar </a>';
+        echo '<a href="../index.php">Voltar </a>';
 
-    //header("location: ../index.php") 
-    //FUNÇÃO DE REDIRECIONAMENTO!!
+        //header("location: ../index.php") 
+        //FUNÇÃO DE REDIRECIONAMENTO!!
+    }
+    else{
+        echo "Olá";
+    }
+
+    
 ?>

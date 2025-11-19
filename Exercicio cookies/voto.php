9ea1ba74cdeeebb8e8253918d994991cb1ebbd27
Milton-Pires <mpnn2207@gmail.com>
2025-11-18 22:18:09 -0300

    Fim da aula + fim att cookies

--- Exercicio cookies/voto.php
+++ Exercicio cookies/voto.php
@@ -1,26 +1,28 @@
+
+<?php
+        $voto = $_GET['voto'];
+
+        if(!isset($_COOKIE['voto'])){
+            setcookie("voto", $voto, time()+(3600*24*31));
+            $msg = "Obrigado por votar!  Seu voto foi: " . $voto;
+        }
+        else{
+            $msg = "<p>Você ja votou nessa enquete!</br> <strong> Seu voto foi: " .$_COOKIE['voto'] . " </strong></p>" ;
+        }
+
+    ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Obrigado!</title>
     <link rel="stylesheet" href="style.css">
 </head>
-<?php
-        $voto = $_GET['voto'];
 
-        if(!isset($_COOKIE['voto'])){
-            setcookie("voto", $voto, time()+ (3600*24*31));
-            $msg = "Obrigado por votar! Seu voto foi: " . $voto;
-        }
-        else{
-            $msg = "<p>Você ja votou nessa enquete! Seu voto foi: " .$_COOKIE['voto'] . "</p>" ;
-        }
-        echo $msg;
-
-    ?>
 <body>
-    
+        <?= $msg ?>
 </body>
 </html>
 

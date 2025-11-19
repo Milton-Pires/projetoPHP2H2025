dc8c5054804156a24ed9f62ce73dc290b1a8600f
Milton-Pires <mpnn2207@gmail.com>
2025-11-18 21:32:33 -0300

    Finalizando atividade cookies

--- Exercicio cookies/index.php
+++ Exercicio cookies/index.php
@@ -4,35 +4,29 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 </head>
 <body>
-    <?php
-        if(isset($_COOKIE['voto'])){
-            
-            $msg = "Você ja votou nessa enquete! seu voto foi: ". $_COOKIES['voto'];
+    <main class="container">
+        <h3>Votação</h3>
+        <form action="voto.php" method="get">
+            <div class="form-check">
+
+                <label for="" class="form-check-label">Escolha o melhor time (na sua opinião)</label> <br>
+                <input type="radio"  name="voto" value="São Paulo"><label>São-Paulo</label> <br>
+                <input type="radio"  name="voto" value="Santos"><label>Santos</label> <br>
+                <input type="radio"  name="voto" value="Palmeiras"><label>Palmeiras</label> <br>
+                <input type="radio"  name="voto" value="Corinthians"><label>Corinthians</label> 
         
-        }
+                <br>
+            </div>
 
-
-    ?>
-    <h3>Votação</h3>
-    <form action="voto.php" method="get">
-        <div class="form-check">
-
-            <label for="" class="form-check-label">Escolha o melhor time (na sua opinião)</label> <br>
-            <input type="radio"  name="voto" value="São Paulo"><label>São-Paulo</label> <br>
-            <input type="radio"  name="voto" value="Santos"><label>Santos</label> <br>
-            <input type="radio"  name="voto" value="Palmeiras"><label>Palmeiras</label> <br>
-            <input type="radio"  name="voto" value="Corinthians"><label>Corinthians</label> 
-        
-            <br>
-        </div>
-
-        <div class="md-3">
-            <button  class="btn btn-primary"  id="enviar"type="submit">Enviar</button>
-    </div>
-    </form>
+            <div class="md-3">
+                <button  class="btn btn-primary"  id="enviar"type="submit">Enviar</button>
+             </div>
+        </form>
+    </main>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </body>
 </html>
--- Exercicio cookies/voto.php
+++ Exercicio cookies/voto.php
@@ -1,13 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
 <?php
         $voto = $_GET['voto'];
 
-        if(isset($_COOKIE['voto'])){
+        if(!isset($_COOKIE['voto'])){
             setcookie("voto", $voto, time()+ (3600*24*31));
             $msg = "Obrigado por votar! Seu voto foi: " . $voto;
         }
         else{
-            $msg = "Você ja votou nessa enquete!";
+            $msg = "<p>Você ja votou nessa enquete! Seu voto foi: " .$_COOKIE['voto'] . "</p>" ;
         }
         echo $msg;
 
     ?>
+<body>
+    
+</body>
+</html>
+
